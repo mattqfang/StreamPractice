56d6ecb0e8af349322708c009a15b120ae78fb71
Unknown <skyesladeut@outlook.com>
2019-10-21 22:43:14 -0600

    Created max, min and count functions

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,29 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-}
+public class Streamer {
+	public int getMax(List<Integer> data) {
+		return data.stream()
+				.max(Integer::compare)
+				.get();
+	}
+	
+	public int getMin(List<Integer> data) {
+		return data.stream()
+				.min(Integer::compare)
+				.get();
+	}
+	
+	public int count(List<Integer> data, Predicate<Integer> condition) {
+		return data.stream()
+				.filter(condition)
+				.collect(Collectors.toList())
+				.size();
+				
+				//.get();
+		}
+		//return 0;
+	}
