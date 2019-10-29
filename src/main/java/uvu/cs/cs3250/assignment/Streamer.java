7d822da58531dca170c9b3c2cfb8287958a4722a
Dallen Baldwin <dallen.baldwin@gmail.com>
2019-10-28 18:04:51 -0600

    jk. adding more to streamer.java for setup purposes

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,4 +1,9 @@
+// I, Dallen Baldwin, assert that this source code was either written by myself
+// or provided by my instructor. For all intents and purposes, it is original.
+
 package uvu.cs.cs3250.assignment;
+
+// Using "Stream" to implement the functionalities in "Streamer" class so as to pass the unit tests.
 
 public class Streamer {
 
