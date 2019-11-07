1cd60288ccf7d9ee3392610117ca50e5712e1fe2
Spencer_Thompson <themalewolf@gmail.com>
2019-11-06 17:02:49 -0700

    Working stream

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,23 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.List;
+import java.util.function.Predicate;
 
+public class Streamer {
+    public int getMax(List<Integer> data) {
+        return data.stream()
+        .max((i,j) -> i.compareTo(j))
+        .get();
+    }
+    public int getMin(List<Integer> data){
+        return data.stream()
+        .min((i,j) -> i.compareTo(j))
+        .get();
+    }
+    public int count(List<Integer> data, Predicate<Integer> cond){
+        return (int) data.stream()
+        .filter(cond)
+        .count();
+        
+    }
 }
