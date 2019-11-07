e3cae38c2eb7a1411e493d9529850d629089d4a9
Nicole Woodard <nwoodard.cs@gmail.com>
2019-11-06 19:40:30 -0700

    Add consistent formatting

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -4,16 +4,17 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Streamer {
+
   public Integer getMax(List<Integer> numbers) {
     return numbers.stream().max(Integer::compare).get();
   }
 
   public Integer getMin(List<Integer> numbers) {
-
     return numbers.stream().min(Integer::compare).get();
   }
 
   public Integer count(List<Integer> numbers, Predicate<Integer> condition) {
     return (int) numbers.stream().filter(condition).count();
   }
+  
 }
--- src/test/java/uvu/cs/cs3250/assignment/StreamerTest.java
+++ src/test/java/uvu/cs/cs3250/assignment/StreamerTest.java
@@ -1,12 +1,11 @@
 package uvu.cs.cs3250.assignment;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
 
 public class StreamerTest {
 	private Streamer streamer;
@@ -36,4 +35,5 @@ public class StreamerTest {
 		int count = streamer.count(data, x -> x % 3 == 0);
 		assertEquals(2, count);
 	}
+	
 }
