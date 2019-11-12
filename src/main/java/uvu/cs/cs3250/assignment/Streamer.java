3feb4d0b48715a9c561496d69219a0d3ddf3748a
Jasen Fonnesbeck <jfsoccerman@hotmail.com>
2019-11-11 20:53:01 -0700

    Passed all tests

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,26 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.List;
+import java.util.function.Predicate;
 
+interface MyFunc {
+	public default int count(List<Integer> ints, Predicate<? super Integer> lamb) {
+		return (int) ints.stream().filter(lamb).count();
+				
+	}
+}
+
+public class Streamer implements MyFunc {
+	
+	public int getMax(List<Integer> ints) {
+		return ints.stream()
+				.max(Integer::compare)
+				.get();
+	}
+	
+	public int getMin(List<Integer> ints) {
+		return ints.stream()
+				.min(Integer::compare)
+				.get();
+	}
 }
