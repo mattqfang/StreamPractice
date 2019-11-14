dfc609fee4d3e74e744b9663135220efefea32d4
Ben Larsen <benjamindlarse@gmail.com>
2019-11-13 18:40:08 -0700

    init

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,34 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
+public class Streamer {
+	Streamer() {
+	}
+
+	public int getMin(List<Integer> nums) {
+		return nums
+			.stream()
+			.sorted()
+			.findFirst()
+			.orElse(null);
+	}
+
+	public int getMax(List<Integer> nums) {
+		return nums
+			.stream()
+			.sorted(Comparator.reverseOrder())
+			.findFirst()
+			.orElse(null);
+	}
+
+	public int count(List<Integer> nums, Predicate<Integer> condition) {
+		return (int) nums
+			.stream()
+			.filter(condition)
+			.count();
+	}
 }
