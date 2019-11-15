b1f952a5e3f27aa87e351cbaa65348c0f8553e50
Kyle Gray <kyeegray@gmail.com>
2019-11-15 14:13:13 -0700

    Functions implemented, tests passed.

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,32 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Streamer {
+
+    public int getMax(List<Integer> list) {
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .getAsInt();
+    }
+
+    public int getMin(List<Integer> list) {
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .min()
+                .getAsInt();
+    }
+
+    public int count(List<Integer> list, Predicate<Integer> condition ) {
+        return list.stream()
+                .filter(condition)
+                .mapToInt(Integer -> 1)
+                .sum();
+
+    }
 
 }
