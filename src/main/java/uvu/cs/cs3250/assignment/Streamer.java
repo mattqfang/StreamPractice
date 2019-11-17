520a9e1962226a238a66c254558656fdac5a3a65
Brandenfascher <jonortim@hotmail.com>
2019-11-16 20:26:35 -0700

    Tests passing
    Streamer class implemented and tests are passing

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,29 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Streamer {
+	
+	public Integer getMax(List<Integer> numberList) {
+		return numberList.stream()
+				.sorted(Comparator.reverseOrder())
+				.findFirst()
+				.orElse(null);
+	}
+	
+	public Integer getMin(List<Integer> numberList) {
+		return numberList.stream()
+				.sorted()
+				.findFirst()
+				.orElse(null);
+	}
+	
+	public Integer count(List<Integer> numberList, Predicate<Integer> predicate) {
+		return (int) numberList.stream()
+				.filter(predicate)
+				.count();
+	}
 
 }
