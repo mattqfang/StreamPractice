f701e3829d520b4b37d7693935a5e37fc98f282a
Delia <10434640@my.uvu.edu>
2019-11-16 17:12:35 -0700

    all tests passedgit add .

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,33 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.List;
 
+public class Streamer {
+	//int getmax (array) 
+	//int get min (array)
+	//getcount (data, x -> x % 3 == 0)
+	List<Integer> a;
+
+	public int getMax(List<Integer> _a) {
+		a = _a;
+		return a.stream()
+				.max(Integer::compareTo)
+				.get();
+	}
+	
+	public int getMin(List<Integer>_a) {
+		a = _a;
+		return a.stream()
+				.min(Integer::compareTo)
+				.get();
+	}
+	
+	public int count(List<Integer>_a, Condition b) {
+		a = _a;
+		return (int)a.stream()
+				.filter(a-> b.isValid(a))
+				.count();
+				
+		
+	}
 }
