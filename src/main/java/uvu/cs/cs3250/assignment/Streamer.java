4b7b7b87a31a5487a4ebc91d9ab8944673fda4e4
parker_mansfield <pmansfield@uvu.edu>
2019-11-18 23:21:15 -0700

    Implemented getMax, getMin, and count functions in Streamer class.

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,30 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+//import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
+public class Streamer {
+	
+	public Streamer() {
+		
+	}
+	
+	public int getMax(List<Integer> intList) {
+		return intList.stream()
+				.max(Integer::compare)
+				.get();
+	}
+	
+	public int getMin(List<Integer> intList) {
+		return intList.stream()
+				.min(Integer::compare)
+				.get();
+	}
+	
+	public int count(List<Integer> intList, Predicate<Integer> pred) {
+		return (int) intList.stream()
+				.filter(pred)
+				.count();
+	}
 }
