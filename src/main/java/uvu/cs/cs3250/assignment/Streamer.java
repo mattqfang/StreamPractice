d95d4fd4ad0975ce45f8bc661ee10147fc2197f0
McKay Larsen <mckay.larsen@microfocus.com>
2019-11-18 18:18:38 -0700

    Generate getMax and getMin functionality

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,17 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+
 public class Streamer {
 
+    public int getMax(List<Integer> data) {
+        return data.stream().max(Integer::compare).get();
+    }
+
+    public int getMin(List<Integer> data) {
+        return data.stream().min(Integer::compare).get();
+    }
+
+    public int count(List<Integer> data, int total) {
+    }
 }
