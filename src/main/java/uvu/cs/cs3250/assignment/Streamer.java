227c108cbecdac93c4c56c3f30bc09bb8e831a25
Cj Davis <c.j._d@hotmail.com>
2019-11-19 13:44:10 -0700

    commiting files to my brach. all 3 test cases passed i believe

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,22 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.*;
+import java.util.function.Predicate;
+
+
 public class Streamer {
+	
+	Integer count(List<Integer> data, Predicate <? super Integer> predicate) {
+		Integer count = (int) data.stream().filter(predicate).count();
+		return count;
+	}
+	Integer getMin(List<Integer> data) {
+		 Integer min = data.stream().min(Integer::compare).get();
+		 return(min);
+	}
+	Integer getMax(List<Integer> data) {
+		 Integer max = data.stream().max(Integer::compare).get();
+		 return(max);
+	}
 
 }
