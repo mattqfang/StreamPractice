38101862493e094cec1474bf17e63d84187b714f
Ryan Trujillo <ryantrujillo@comcast.net>
2019-11-20 14:51:57 -0700

    Final submission

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,28 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+public class Streamer {
+	
+	// Get the maximum value from a list.
+	int getMax(List<Integer> data) {
+		return data.stream()
+				.collect(Collectors.maxBy(Comparator.naturalOrder()))
+				.orElse(0);
+	}
+	
+	int getMin(List<Integer> data) {
+		return data.stream()
+				.collect(Collectors.minBy(Comparator.naturalOrder()))
+				.orElse(0);
+	}
+	
+	int count(List<Integer> data, Predicate<Integer> condition) {
+		return data.stream()
+				.filter(condition)
+				.collect(Collectors.counting()).intValue();
+	}
 }
