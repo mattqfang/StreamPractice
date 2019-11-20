19f7f27f15e161d0327390dd652ee5c4d5589400
Tyler Banks <tyler.banks256@gmail.com>
2019-11-19 22:38:57 -0700

    Passed unit tests.

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,35 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 public class Streamer {
+
+	public int getMax(List<Integer> input) 
+	{
+		int result = input.stream()
+				.max((x, y) -> x - y)
+				.get();
+		
+		return result;
+	}
+
+	public int getMin(List<Integer> input) 
+	{
+		int result = input.stream()
+				.min((x, y) -> x - y)
+				.get();
+		
+		return result;
+	}
+
+	public int count(List<Integer> input, Predicate<Integer> obj)
+	{
+		long result = input.stream()
+				.filter(obj)
+				.count();
+		return (int)result;
+	}
 
 }
