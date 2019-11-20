1c97cdf8b8666141b6d0c26896b3e5ef9bbfb431
Jordan <frigis.something@gmail.com>
2019-11-20 15:41:00 -0700

    Finished streamer project

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,27 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
+public class Streamer {
+	public int getMax(List<Integer> data) {
+		return data.stream()
+				.sorted(Comparator.reverseOrder())
+				.findFirst()
+				.get();
+	}
+	
+	public int getMin(List<Integer> data) {
+		return data.stream()
+				.sorted()
+				.findFirst()
+				.get();
+	}
+		
+	public int count(List<Integer> data, Predicate<Integer> t) {
+		return (int) data.stream()
+				.filter(t)
+				.count();
+	}
 }
