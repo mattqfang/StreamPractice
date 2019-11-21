cc04c6207b2643c5e6e8261e8016b6fb19a72252
Aaron Bishop <acbishop13@gmail.com>
2019-11-20 20:25:10 -0700

    Streamer class passes all tests.

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,27 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
+public class Streamer {
+    public static int getMax(List<Integer> numbers){
+        return numbers.stream()
+                .sorted(Comparator.reverseOrder())
+                .findFirst()
+                .get();
+    }
+
+    public static int getMin(List<Integer> numbers){
+        return numbers.stream()
+                .sorted(Comparator.naturalOrder())
+                .findFirst()
+                .get();
+    }
+
+    public static int count(List<Integer> numbers, Predicate<Integer> p){
+        return (int) numbers.stream()
+                .filter(p)
+                .count();
+    }
 }
