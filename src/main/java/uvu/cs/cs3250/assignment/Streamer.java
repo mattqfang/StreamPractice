3fb323c3234f4434b0841837211fd9931a30c06f
Cory <10764424@my.uvu.edu>
2019-11-20 21:44:01 -0700

    Update Streamer.java
    Assignment submit

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,33 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+
 public class Streamer {
+
+	public int getMax(List<Integer> data) {
+		int max = Collections.max(data);
+		return max;
+	}
+
+	public int getMin(List<Integer> data) {
+		int min = Collections.min(data);
+		return min;
+	}
+
+	public int count(List<Integer> data, logic var) {
+		int result = 0;
+		for(Integer item: data) {
+			if(var.calculate(item)) {
+				result++;
+			}
+		}
+		return result;
+	}
+	//interface
+	interface logic{
+		boolean calculate(Integer item);
+	}
 
 }
