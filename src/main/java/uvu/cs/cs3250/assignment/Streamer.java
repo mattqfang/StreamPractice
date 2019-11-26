ada47a0c6c2205135fea33cfbacd0c35ac50764f
sssindustries <sssindustries@me.com>
2019-11-25 18:37:35 -0700

    Update Streamer.java

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,19 @@
 package uvu.cs.cs3250.assignment;
 
-public class Streamer {
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+public class Streamer {
+	public Integer getMax(List<Integer> data) {
+		return data.stream().max(Integer::compare).get();
+	}
+	
+	public Integer getMin(List<Integer> data) {
+		return data.stream().min(Integer::compare).get();
+	}
+	
+	public Integer count(List<Integer> data, Predicate<Integer> predicate) {
+		return (int) data.stream().filter(predicate).count();
+	}
 }
