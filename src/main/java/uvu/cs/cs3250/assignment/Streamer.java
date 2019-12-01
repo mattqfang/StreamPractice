2b03ccd5b0ac8d9038d54f928e841ec0bdeff579
Candra Ray <candraray@hotmail.com>
2019-12-01 07:42:43 -0700

    functions completed, tests passsed

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,19 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Streamer {
 
+	public int getMax(List<Integer> data) {
+		return data.stream().max(Integer::compare).get();
+	}
+	
+	public int getMin(List<Integer> data) {
+		return data.stream().min(Integer::compare).get();
+	}
+	
+	public int count(List<Integer> data, Predicate<Integer> condition) {
+		return (int) data.stream().filter(condition).count();
+	}
 }
