40022d1e1b2cb32b74a2a382a8e2ac4a615140df
zachjareth@gmail.com <zachjareth@gmail.com>
2019-12-04 17:19:52 -0700

    Update Streamer.java
    Updated stream class with required methods so that tests passed - ZH

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,36 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class Streamer {
 
+	
+	public int getMax(List<Integer> intData) {
+		int maxInt = 0;
+		
+		maxInt = intData.stream()
+						.max(Integer::compare)
+						.get();
+		return maxInt;
+	}
+	
+	public int getMin(List<Integer> intData) {
+		int minInt = 0;
+		
+		minInt = intData.stream()
+						.min(Integer::compare)
+						.get();
+		return minInt;
+	}
+	
+	public int count(List<Integer> intData, Predicate<Integer> countCondition) {
+		int sumInt = 0;
+		
+		sumInt = (int) intData.stream()
+						.filter(countCondition)
+						.count();
+		return sumInt;
+	}
 }
