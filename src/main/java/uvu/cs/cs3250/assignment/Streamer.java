c28b5867cdf5b5e82536a924b260e3cd704863ee
Michael <mike,shipleyland@gmail.com>
2019-12-07 12:51:52 -0700

    Added Stream Methods
    Streamer methods testGetMax, testGetMin, testCountWithCondition were added.

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,27 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Streamer {
+
+	public int getMax(List<Integer> data) {
+		return data.stream()
+				.max(Integer::compare)
+				.get();
+	}
+
+	public int getMin(List<Integer> data) {
+		// TODO Auto-generated method stub
+		return data.stream()
+				.min(Integer::compare)
+				.get();
+	}
+
+	public int count(List<Integer> data, Predicate<? super Integer> condition) {
+		return (int) data.stream()
+			.filter(condition) //add one to the iterator that is returned at the end that indicates how many items in the list (parameter 1) that meet the condition (parameter 2).
+			.count();
+	}
 
 }
