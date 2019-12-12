0dd04f8b36ae43f4b13ae74e36a415c414855c03
Braden Sorensen <10457696@uvu.edu>
2019-12-11 21:09:02 -0700

    This is done now.

--- src/main/java/uvu/cs/cs3250/assignment/Streamer.java
+++ src/main/java/uvu/cs/cs3250/assignment/Streamer.java
@@ -1,5 +1,34 @@
 package uvu.cs.cs3250.assignment;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+interface Mod3 {
+	boolean test(int x);
+}
+
 public class Streamer {
+	public int getMax(List<Integer> data) {
+		return data
+				.stream()
+				.max(Integer::compare)
+				.get();
+	}
+	
+	public int getMin(List<Integer> data) {
+		return data
+				.stream()
+				.min(Integer::compare)
+				.get();
+	}
+	
+	public int count(List<Integer> data, Mod3 canDivide) {
+		List<Integer> Threes = data
+				.stream()
+				.filter(x -> canDivide.test(x))
+				.collect(Collectors.toList());
+		return Threes.size();
+	}
+
 
 }
